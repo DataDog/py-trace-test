5e9ecf7bc2462289d5b7f04563c253aacf9605ed
bburns-ddog <29928783+burnsie7@users.noreply.github.com>
2023-10-11 14:09:38 -0700

    add spark ext

---
+++ ddtrace/ext/spark.py
@@ -0,0 +1,10 @@
+# defaults
+APP = "spark"
+DEFAULT_SERVICE = "spark"
+# standard tag
+RAWCMD = "spark.application"
+CMD = "spark.application"
+STAGE = "spark.stage"
+JOB = "spark.job"
+SQL = "spark.sql"
+TASK = "spark.task"
